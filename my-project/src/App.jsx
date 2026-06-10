22e5f72fbcb43a99fe68297c1c336071592c6b94
Tejas Saraf <tejas@thekiranacademy.com>
2026-06-10 18:05:26 +0530

    counter

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -34,15 +34,25 @@ function App() {
 
         <h1>
           {isActive ? "User is Logged-in" : "User is Logged-out"}
+
         </h1>
+
+
 
         <button className='one' onClick={handeClick}>Click Me</button>
 
-        <CounterApp/>
-     
-     <MobileComp/>
+        <CounterApp />
 
-     
+        <br />
+        <br />
+        <br />
+
+     <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolore eius veritatis, sequi, iste accusamus, cumque perspiciatis odit obcaecati laudantium culpa? Error fugit libero necessitatibus amet iste, dignissimos culpa qui odit eaque magni beatae exercitationem reiciendis, impedit nihil doloribus soluta tempore aut eveniet praesentium incidunt. Repellendus cupiditate debitis voluptatem explicabo similique! Tenetur eligendi quam, dicta, rem maiores enim reprehenderit aperiam sint veritatis aspernatur hic nostrum facere beatae! Dolorem tenetur sint quisquam, reiciendis soluta ad nam? Nemo animi delectus tempora maiores sint odit facilis quam sed illum commodi magni suscipit aliquam fuga dolore dolor, labore debitis sit adipisci quibusdam eius aut nobis. Reprehenderit rerum fuga totam ullam obcaecati numquam, itaque iste non deserunt. Explicabo nobis, veritatis quia perspiciatis maiores nam maxime alias in officia sint nulla aut sed deserunt ex debitis aliquam ut eaque neque fuga! Dignissimos asperiores temporibus debitis excepturi perspiciatis natus quisquam earum veniam eaque ab cumque harum illo eligendi quas ex nulla, minima nemo odio? Quam repellendus quibusdam eligendi, fugit deleniti temporibus dolorum aspernatur deserunt facilis quidem? Ducimus sint minus debitis vero laborum. Quod expedita odit vitae nobis quis ratione voluptas omnis tempore. Dolor nemo distinctio voluptatum assumenda, possimus inventore explicabo suscipit mollitia provident iste consequuntur pariatur dignissimos?
+     </div>
+
+
+
       </center>
     </>
   )
--- my-project/src/CounterApp.jsx
+++ my-project/src/CounterApp.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CounterApp = () => {
+
+    let count = 0;
+
+    const [scount, setScount] = useState(0)   // hook
+
+    const handleIncrease = () => {
+
+        count++;
+        console.log("normal var = ", count)
+
+        setScount(scount + 1)
+        console.log("dynamic var = ", scount)
+
+    }
+    const handleDecrease = () => {
+
+        count--;
+        console.log("normal var = ", count)
+
+        setScount(scount - 1)
+        console.log("dynamic var = ", scount)
+
+    }
     return (
         <div>
 
             <h1>
                 Hello I am From Counter App
             </h1>
+
+            <h2>
+                Normal Count = {count} {" | "} State Count = {scount}
+
+            </h2>
+            {/* <button onClick={handleIncrease}>Increase</button> <br /> <br /> */}
+            <button onClick={() => { setScount(scount + 1) }}>Increase</button> <br /> <br />
+
+            {/* <button onClick={handleDecrease}>Decrease</button> <br /> <br /> */}
+            <button onClick={() => { setScount(scount - 1) }}>Decrease</button> <br /> <br />
+
+            <button onClick={() => { setScount(scount * 0) }}>Reset</button> <br /> <br />
         </div>
     )
 }
